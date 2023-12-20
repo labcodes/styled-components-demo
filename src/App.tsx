a92f48468e459bf1b29fd311913042badb445734
HigorMonteiro <higorvmonteiro@gmail.com>
2023-12-20 09:29:01 -0300

    adding effects to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-// src/App.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import styled from 'styled-components';
+import Confetti from 'react-confetti';
 
-const AppContainer = styled.div`
+const AppContainer = styled.div<{bgColor: string}>`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh; // Viewport Height
+  height: 100vh;
+  background-color: ${props => props.bgColor};
 `;
 
 const App: React.FC = () => {
+  const [confetti, setConfetti] = useState(false);
+  const [bgColor, setBgColor] = useState('white');
+
   const handleClick = () => {
+    setConfetti(true);
+    setBgColor('#e3d1fa')
+    setTimeout(() =>{
+      setConfetti(false)
+      setBgColor('white');
+      }, 5000);
+
     console.log('Button clicked!');
   };
 
   return (
-    <AppContainer>
+    <AppContainer bgColor={bgColor}>
+      {confetti && <Confetti />}
       <Button text="Click Me" onClick={handleClick} />
     </AppContainer>
   );
--- src/StyledButton.tsx
+++ src/StyledButton.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  background-color: blue;
+  background-color: #935fd3;
   color: white;
   padding: 10px 20px;
   border: none;
@@ -11,13 +11,13 @@ const StyledButton = styled.button`
   transition: all 0.3s ease;
 
   &:hover {
-    background-color: darkblue;
+    background-color: #5a3489;
     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
   }
 
   &:focus {
     outline: none;
-    box-shadow: 0 0 0 3px lightblue;
+    box-shadow: 0 0 0 3px #c7a3f5;
   }
 `;
 
